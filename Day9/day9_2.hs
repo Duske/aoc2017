bed4c42cf8befb6d1a12d647b5dc86dc1857f9ac
Dustin Chabrowski <dustin.duske@gmail.com>
2017-12-09 11:58:02 +0100

    [TASK] add day9_2

---
+++ Day9/day9_2.hs
@@ -0,0 +1,15 @@
+module Day9_1 where
+
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  putStr $ show $ calc input False
+
+calc :: String -> Bool -> Int
+calc ('!':_:rest) garbage =  calc rest garbage -- the skip operator needs to fetch with highest priority
+calc ('>':rest) _ =  calc rest False -- end garbage operator before garbage catchall
+calc (_:rest) True = 1 + calc rest True -- catch all chars while in garbage "mode" and plus one
+calc ('<':rest) False = calc rest True -- start garbage "mode" when not active
+calc (_:rest) False =  calc rest False -- catch non-operator chars like 'a,b,c...' in non-garbage "mode"
+calc [] _  = 0 -- return result
