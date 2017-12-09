9f48b683767382e7b9f4d085d4753c970c984269
Dustin Chabrowski <dustin.duske@gmail.com>
2017-12-09 11:52:44 +0100

    [REFAC] day9_1

--- Day9/day9_1.hs
+++ Day9/day9_1.hs
@@ -7,10 +7,11 @@ main = do
   putStr $ show $ calc input 0 0 False
 
 calc :: String -> Int -> Int -> Bool -> Int
-calc ('{':rest) level result garbage = if garbage then calc rest level result garbage else calc rest (level + 1) result garbage
-calc ('}':rest) level result garbage = if garbage then calc rest level result garbage else calc rest (level - 1) (result + level) garbage
-calc ('<':rest) level result garbage = if garbage then calc rest level result garbage else calc rest level result True
-calc ('>':rest) level result _ =  calc rest level result False
-calc ('!':_:rest) level result garbage =  calc rest level result garbage
-calc (_:rest) level result garbage =  calc rest level result garbage
-calc [] _ result _  = result
+calc ('!':_:rest) level result garbage =  calc rest level result garbage -- the skip operator needs to fetch with highest priority
+calc ('>':rest) level result _ =  calc rest level result False -- end garbage operator before garbage catchall
+calc (_:rest) level result True = calc rest level result True -- catch all chars while in garbage "mode"
+calc ('{':rest) level result False = calc rest (level + 1) result False  -- increase level
+calc ('}':rest) level result False = calc rest (level - 1) (result + level) False -- decrease level but increase the result
+calc ('<':rest) level result False = calc rest level result True -- start garbage "mode" when not active
+calc (_:rest) level result False =  calc rest level result False -- catch non-operator chars like 'a,b,c...'
+calc [] _ result _  = result -- return result
